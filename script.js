4d32b65627c172293b19110a6d31ebbd800140d3
Santhoshi <san.lakh1306@gmail.com>
2023-12-01 00:46:28 -0500

    done

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
     const title = document.querySelector("#overlay1");
     const start = document.querySelector("#start");
     const inst = document.querySelector("#overlay2");
+    const content = document.querySelector("#content");
+    const ina = document.querySelector("#ina");
+    const access = document.querySelector("#access");
 
     title.addEventListener("click",  function(event){
         event.preventDefault();
@@ -14,5 +17,10 @@
     start.addEventListener("click", function(event){
         event.preventDefault();
         inst.className = "hide";
+    })
+    content.addEventListener("click", function(event){
+        event.preventDefault();
+        ina.className = "hide";
+        access.className = "show";
     })
 })();
